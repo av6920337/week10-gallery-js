048f0349f38db7722bba4a93b16d07cd613c96cc
av6920337 <av6920337@gmail.com>
2023-12-29 14:29:56 +0100

    The entire HTML page structure has been created.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # week10-gallery-js
+
+Hi! It's my first work in JS.
+A JS gallery was created in this repository.
---
+++ assets/scripts/index.js
@@ -0,0 +1,16 @@
+// function sayHi() {
+//     console.log('Я учу JavaScript!'); 
+// }
+// const sayHi=()=>{
+//     console.log('Я учу JavaScript!');
+// };
+
+const img = document.querySelector('.images');
+console.log(img)
+
+const newFoto =()=>{
+    img.src="../assets/images/img2.jpg";
+}  
+const oldFoto =()=>{
+    img.src="../assets/images/img1.jpg"
+}
